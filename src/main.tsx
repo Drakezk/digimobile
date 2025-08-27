6d2f50ba48697a71d6321f262944fbff423d6454
Drakezk <khoi30925@gmail.com>
2025-08-27 20:30:46 +0700

    Fix router basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/digimobile">
       <QueryClientProvider client={queryClient}>
         <UserProvider>
           <CartProvider>
